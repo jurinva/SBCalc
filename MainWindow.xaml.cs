231e839c9579ce18d20523419bb9e163eaa74248
Vachounet <vachounet@live.fr>
2015-04-10 13:58:23 +0200

    Initial Import

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SBCalc
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void btnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            if ((txtCPUID.Text != null) && (txtCPUID.Text.Length >= 15) && (txtCPUID.Text.Length <= 16))
+            {
+                string SBKCodeResult = "";
+                int l_sbkResult = SBKLibrary.GetEncoder(this.txtCPUID.Text, ref SBKCodeResult);
+                if (SBKCodeResult != null && SBKCodeResult != "")
+                {
+                    char[] rmChar = { '\\', '0' };
+                    this.txtSBK.Text = SBKCodeResult.Remove((SBKCodeResult.Length - 1), 1);
+                }
+            }
+            else
+                MessageBox.Show("Please, check your CPUID.", "CPUID Error");
+        }
+    }
+}
---
+++ SBKLibrary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace SBCalc
+{
+    public static class SBKLibrary
+    {
+        [DllImport("Pica_Func.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi, EntryPoint = "?Encode@Pica@pica_lib@@SAHPADH0H@Z", ExactSpelling = true)]
+        public unsafe static extern int Encode(byte* input, int isize, byte* output, int osize);
+        public unsafe static int GetEncoder(string CPUID, ref string SBKCodeResult)
+        {
+            byte[] array = new byte[44];
+            byte[] array2 = new byte[CPUID.Length];
+            for (int i = 0; i < CPUID.Length; i++)
+            {
+                array2[i] = (byte)CPUID[i];
+            }
+            int result;
+            fixed (byte* ptr = array)
+            {
+                fixed (byte* ptr2 = array2)
+                {
+                    result = SBKLibrary.Encode(ptr2, array2.Length, ptr, array.Length);
+                    for (int j = 0; j < array.Length; j++)
+                    {
+                        SBKCodeResult += Convert.ToChar(array[j]);
+                    }
+                }
+            }
+            return result;
+        }
+    }
+
+}
